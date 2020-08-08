66bf74c0d6e789f3e73322ea7986015723b4a1f5
erimal <emyadodo12@gmail.com>
2020-08-08 23:02:07 +0000

    DB settings change to django envi

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
-#SMS settings
-SMS_SYSTEM_HOSTNAME = '10'
-SMS_SYSTEM_PORT = '1234'
-SMS_SYSTEM_ID = '9876'
-SMS_SYSTEM_PASSWORD= 'mytestpassowrd'
-
-#DB Settings
+# #SMS settings
+# SMS_SYSTEM_HOSTNAME = '10'
+# SMS_SYSTEM_PORT = '1234'
+# SMS_SYSTEM_ID = '9876'
+# SMS_SYSTEM_PASSWORD= 'mytestpassowrd'
+#
+# #DB Settings
 
 DB_SERVER = 'localhost'
-DB_DATABASE = 'sms'
+DB_DATABASE = 'smsdev'
 DB_USERNAME = 'erimal'
 DB_PASSWORD = 'Turniphair!30'
 
